e0059903ae51f11b8c0118ea24010c096a5463b6
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-03-11 21:36:51 +0100

    Server setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+//* Init server
+const app = express();
+
+//* Database connection
+mongoose.connect( process.env.mongoURI , {useUnifiedTopology: true, useNewUrlParser: true});
+const db = mongoose.connection;
+
+//* Middlewares
+app.use(express.json());
+
+//* Cors enabled
+const allowedOrigins = ['http://localhost:3000/'];
+app.use(cors({
+    origin: allowedOrigins
+}));
+
+//* Routes
+app.use('/', require('./routes/watchlists'));
+
+//* Static files serve
+let dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname, '/client/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(dirname, 'client', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send(`API is running.... in ${process.env.NODE_ENV} mode`);
+  });
+}
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database!'));
+
+//* Running server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}, on ${process.env.NODE_ENV}mode!`));
